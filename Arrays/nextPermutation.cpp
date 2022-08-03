bd17f859ce86f2d0a9d5aa5d28fb885037b40960
attadeep28 <92106565+attadeep28@users.noreply.github.com>
2022-08-03 08:32:41 +0530

    message: Added nextPermutation

--- Arrays/nextPermutation.cpp
+++ Arrays/nextPermutation.cpp
@@ -3,6 +3,26 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+        ex
+        a[] = 1 3 5 4 2
+        ind = 0 1 2 3 4
+
+        step 1: start traversing from back and find first idx such that a[i] < a[i+1]
+                ind1 = 1
+
+        step 2: again start traversing from back and find a[ind2] > a[ind1]
+                ind2 = 3
+
+        step 3: swap(a[ind1],a[ind2])
+                now our Array will be
+            a[i]:    1 4 5 3 2
+              i :    0 1 2 3 4
+
+        step 4: reverse(a[ind1 + 1] to last)
+            a[i]: 1 4 2 3 5
+              i : 0 1 2 3 4
+    */
     void nextPermutation(vector<int> &nums)
     {
         int n = nums.size();
@@ -29,14 +49,45 @@ public:
             reverse(nums.begin() + k + 1, nums.end());
         }
     }
+
+    // Brute force generate all permutaion
+    // TC O(n!)
+    void permute(vector<int> a, int l, int r)
+    {
+        // Base case
+        if (l == r)
+        {
+            for (auto it : a)
+                cout << it << " ";
+            cout << endl;
+        }
+        else
+        {
+            // Permutations made
+            for (int i = l; i <= r; i++)
+            {
+
+                // Swapping done
+                swap(a[l], a[i]);
+
+                // Recursion called
+                permute(a, l + 1, r);
+
+                // backtrack
+                swap(a[l], a[i]);
+            }
+        }
+    }
 };
 
 int main()
 {
     Solution sol;
     vector<int> per = {1, 2, 3};
-    sol.nextPermutation(per);
-    for (auto it : per)
-        cout << it << " ";
+    sol.permute(per, 0, 2);
+
+    // sol.nextPermutation(per);
+    // for (auto it : per)
+    //     cout << it << " ";
     return 0;
 }
